fce33608088717b09a416d0ae49f41b664bff7b7
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-07-17 09:36:32 -0600

    Add the mechanics model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200717152124) do
+ActiveRecord::Schema.define(version: 20200717153335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "mechanics", force: :cascade do |t|
+    t.string "name"
+    t.integer "years_of_experience"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "parks", force: :cascade do |t|
     t.string "name"
